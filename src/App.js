569438c2abdbb581380844564f77a8178c89c0be
shivanigotavade <42857225+shivanigotavade@users.noreply.github.com>
2024-03-27 12:11:44 +0530

    working

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import WebcamCapture from './WebCam';
+import FaceWevCam from './FaceWebCam'
+
+function App() {
+  return (
+    <div>
+<WebcamCapture/>
+
+    </div>
+   
+  );
+}
+
+export default App;
