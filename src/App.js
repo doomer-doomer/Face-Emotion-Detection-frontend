e104e25ca5b806532c98de251334f8f36d0f5033
shivanigotavade <42857225+shivanigotavade@users.noreply.github.com>
2024-06-26 19:54:29 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import WebcamCapture from './WebCam';
 import FaceWevCam from './FaceWebCam'
+import Spotify from './Spotify';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div>
-<WebcamCapture/>
+
+<Spotify />
+
 
     </div>
    
